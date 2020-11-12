deb9978b637761669449d5599dcaa4bfa46eb052
Artem721 <71816794+Artem721@users.noreply.github.com>
2020-11-12 12:14:18 +0300

    Add files via upload

---
+++ ft_pos_neg_separator_lst.py
@@ -0,0 +1,29 @@
+def ft_len(b):
+    s = 0
+    for i in b:
+        b += 1
+    return b
+
+
+def ft_odd_even_separator_lst(x):
+    a = 0
+    for i in x:
+        a += 1
+    vt = a
+    i = 0
+    t = []
+    nc = []
+    num = []
+    com = [[0], [0]]
+    for i in range(vt):
+        if x[i] > 0:
+            t.append(x[i])
+        elif x[i] < 0:
+            nc.append(x[i])
+        else:
+            num.append(x[i])
+        i += 1
+    com[0] = nc
+    com[1] = num
+    com[2] = c
+    return com
